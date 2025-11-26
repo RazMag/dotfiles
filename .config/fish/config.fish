f633d2800760e13c6d422647dda6d167ccd39b37
razmag <46993372+RazMag@users.noreply.github.com>
2025-11-26 22:49:47 +0200

    initial

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# Interactive block
+if status is-interactive
+    alias hx="helix"
+end
