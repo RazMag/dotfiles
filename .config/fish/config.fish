335fbd4e761d0505539f7b1b9aa997d1fb90a6d9
razmag <46993372+RazMag@users.noreply.github.com>
2025-11-27 17:50:56 +0200

    feat: added integrations to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,4 +9,6 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # Interactive block
 if status is-interactive
     alias hx="helix"
+    atuin init fish | source
+    zoxide init fish --cmd cd | source
 end
